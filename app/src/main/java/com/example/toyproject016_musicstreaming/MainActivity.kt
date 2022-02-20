b2bf208ddf1b0bd3c83e00bfddab16332b6e2be1
kimjinsub <hikm9985@naver.com>
2022-02-20 21:01:23 +0900

    music Streaming♬  UI 구성

--- app/src/main/java/com/example/toyproject016_musicstreaming/MainActivity.kt
+++ app/src/main/java/com/example/toyproject016_musicstreaming/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.toyproject016_musicstreaming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.Player
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer,PlayerFragment.newInstance())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/toyproject016_musicstreaming/PlayerFragment.kt
@@ -0,0 +1,18 @@
+package com.example.toyproject016_musicstreaming
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment: Fragment(R.layout.fragment_player) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance(): PlayerFragment{
+            return PlayerFragment()
+        }
+    }
+}
